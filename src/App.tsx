ca72708d1b3d97bdc30e59ecadf1e78b66671e7b
Jophret BAKANA <164746743+bjd3355@users.noreply.github.com>
2025-03-26 10:30:45 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './styles/output.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Bienvenue sur <span className="text-red-500">EDUTECH</span>
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
